b0f19c6d69900bde3cb9a6a431fd6204daa30608
DiegooNP <diegopaez73@gmail>
2023-01-11 19:10:17 -0300

    subiendo clase numero 3

---
+++ micro1.js
@@ -0,0 +1,18 @@
+// Venta de boletos para ingresar a un partido de futbol
+
+let nombre = "Otto";
+let apellido = "Paez";
+let edad = 2;
+let numeroTelefono = "2444555555";
+let socio = true;
+let fechaPartido = "22-11-22";
+let numeroSilla = 24;
+let precioBoleto = 500.50;
+
+console.log(typeof(nombre), nombre);
+console.log(typeof(apellido), apellido);
+console.log(typeof(edad), edad);
+console.log(typeof(numeroTelefono), numeroTelefono);
+console.log(typeof(socio), socio);
+console.log(typeof(fechaPartido), fechaPartido);
+console.log(typeof(numeroSilla), numeroSilla);
